18175ba7cacd65a40dae6e797580849ba1a06a01
Xingwang Liao <kuoruan@gmail.com>
2021-03-26 23:30:20 +0800

    feat(fetch): pass arguments directly to go fetch

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -1,7 +1,6 @@
 import Response from "./Response";
 
 async function fetch(url, opt) {
-  if (typeof url !== "string") url = String(url);
   /* global _goFetchSync */
   const res = await _goFetchSync(url, opt);
   return new Response(res);
