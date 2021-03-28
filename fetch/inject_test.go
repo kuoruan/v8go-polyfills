4cdc982d5e3320201e9eea23fac8160fa53a72a5
Xingwang Liao <kuoruan@gmail.com>
2021-03-28 11:17:17 +0800

    test(fetch): add inject test

---
+++ fetch/inject_test.go
@@ -0,0 +1,83 @@
+package fetch
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"rogchap.com/v8go"
+)
+
+func TestInjectTo(t *testing.T) {
+	t.Parallel()
+
+	iso, _ := v8go.NewIsolate()
+	global, _ := v8go.NewObjectTemplate(iso)
+
+	if err := InjectTo(iso, global); err != nil {
+		t.Errorf("error when inject fetch polyfill, %s", err)
+		return
+	}
+
+	ctx, err := v8go.NewContext(iso, global)
+	if err != nil {
+		t.Errorf("create context failed: %s", err)
+		return
+	}
+
+	val, err := ctx.RunScript("fetch('https://www.example.com')", "fetch_example.js")
+	if err != nil {
+		t.Errorf("failed to do fetch test: %s", err)
+		return
+	}
+
+	pro, err := val.AsPromise()
+	if err != nil {
+		t.Errorf("can't convert to promise object: %s", err)
+		return
+	}
+
+	done := make(chan bool, 1)
+	go func() {
+		for pro.State() == v8go.Pending {
+			continue
+		}
+
+		done <- true
+	}()
+
+	select {
+	case <-time.After(time.Second * 10):
+		t.Errorf("request timeout")
+		return
+	case <-done:
+		if pro.State() != v8go.Fulfilled {
+			t.Errorf("should fetch success, but not")
+			return
+		}
+	}
+
+	obj, err := pro.Value.AsObject()
+	if err != nil {
+		t.Errorf("can't convert fetch result to object, %s", err)
+		return
+	}
+
+	str, err := v8go.JSONStringify(ctx, obj)
+	if err != nil {
+		t.Errorf("can't stringify object: %s", err)
+		return
+	}
+
+	fmt.Println(str)
+
+	ok, err := obj.Get("ok")
+	if err != nil {
+		t.Errorf("get object 'ok' failed: %s", err)
+		return
+	}
+
+	if !ok.Boolean() {
+		t.Error("should be ok, but not")
+	}
+}
