a8303dfe4693e407f8ddb279024dba6f70ddc16a
Xingwang Liao <kuoruan@gmail.com>
2021-03-29 10:59:31 +0800

    chore: update feature-test.js

--- feature-test.js
+++ feature-test.js
@@ -2,4 +2,6 @@ console.log("URL: " + typeof URL);
 console.log("URLSearchParams: " + typeof URLSearchParams);
 
 console.log("fetch: " + typeof fetch);
-console.log("Response: " + typeof Response);
+
+console.log("atob: " + typeof atob);
+console.log("btoa: " + typeof btoa);
